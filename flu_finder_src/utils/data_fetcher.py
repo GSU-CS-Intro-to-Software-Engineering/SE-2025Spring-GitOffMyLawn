c76d70c2c7260ea9daf549838f64078ec2d6abfd
David Stepanov <davidstepanov@yahoo.com>
2025-03-09 18:23:48 -0400

    Updated indexing

--- flu_finder_src/utils/data_fetcher.py
+++ flu_finder_src/utils/data_fetcher.py
@@ -4,7 +4,6 @@ import requests
 import getpass
 import polars as pl
 from dotenv import load_dotenv
-from pathlib import Path
 
 # CSV URL
 csv_url = "https://www.cdc.gov/bird-flu/modules/situation-summary/commercial-backyard-flocks.csv"
@@ -12,15 +11,18 @@ csv_url = "https://www.cdc.gov/bird-flu/modules/situation-summary/commercial-bac
 # Load environment variables from .env
 load_dotenv()
 
-# Download path that comes from .env file
-download_path = Path(os.getenv("DOWNLOAD_PATH"))
+# Get download path from .env (default to project root if not set)
+download_path = os.getenv("DOWNLOAD_PATH", os.path.join(os.getcwd(), "data.csv"))
+
 
 # Download the CSV file
 def download_csv():
+
     print(f"Saving file to: {download_path}")
     response = requests.get(csv_url)
     if response.status_code == 200:
-        download_path.write_bytes(response.content)
+        with open(download_path, "wb") as file:
+            file.write(response.content)
         print(f"CSV file downloaded to {download_path}")
     else:
         print("Couldn't download CSV")
@@ -28,16 +30,14 @@ def download_csv():
 
 def get_sorted_dataframe():
     # Load the CSV into Polars
-    df = pl.read_csv(download_path, try_parse_dates=True, row_index_name="index")
+    df = pl.read_csv(download_path, try_parse_dates=True)
 
     # Convert "Outbreak Date" to Date type and sort
     df_sorted = df.with_columns(pl.col("Outbreak Date").str.strptime(pl.Date, "%m-%d-%Y")).sort("Outbreak Date")
+    df_sorted = df_sorted.with_row_index("index")
 
     # Configure Polars to display all rows
     pl.Config.set_tbl_rows(len(df_sorted))
 
     """Returns the loaded DataFrame."""
     return df_sorted
-
-if __name__ == "__main__":
-    download_csv()  # Ensure script runs when executed
