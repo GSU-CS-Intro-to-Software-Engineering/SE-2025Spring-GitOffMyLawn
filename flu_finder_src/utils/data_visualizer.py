0abdec2a77f2ee94c7d0f415053d35b292fe553f
David <davidstepanov@yahoo.com>
2025-03-23 11:57:41 -0400

    Default dragmode is pan instead of zoom. Removed useless modeBarButtons

--- flu_finder_src/utils/data_visualizer.py
+++ flu_finder_src/utils/data_visualizer.py
@@ -35,11 +35,14 @@ def line_graph_maker(df, output_file="outbreak_plot.html"):
         yaxis_title="Flock Size",
         title_x=0.5,
         hoverlabel=dict(bgcolor="white", font_size=12),
-        template="plotly_white"
+        template="plotly_white",
+        dragmode="pan"
     )
     
     # Scroll wheel zoom and always visible modebar
-    config = {"displayModeBar": True, "scrollZoom": True}
+    config = {"displayModeBar": True,
+              "scrollZoom": True,
+              "modeBarButtonsToRemove": ["autoScale", "select2d", "lasso2d"]}
 
     # Save to HTML
     fig.write_html(output_file, config=config)
@@ -48,9 +51,9 @@ def line_graph_maker(df, output_file="outbreak_plot.html"):
 
 #------------------------------------------- Method Testing -----------------------------------------#
 if __name__ == "__main__":
-    # frame = set_time_frame("2022", "2030")                           # <== National
+    frame = set_time_frame("2022", "2030")                           # <== National
     # frame = set_time_frame("2025-01-01", "2025-02-01", "Georgia")    # <== State
-    frame = set_time_frame("2024", "2030", "ioWA", "BUENA VistA")    # <== County
+    # frame = set_time_frame("2024", "2030", "ioWA", "BUENA VistA")    # <== County
     summed_frame = sum_by_date(frame)
     line_graph_maker(summed_frame)
     
