33263cccb901576cbcb6ecd7d9e143da0c4dfc6d
oscarmejia1776 <oscar.mejia17@gmail.com>
2025-03-25 14:14:37 -0400

    port handling for deployment/local

--- flu_finder_src/app.py
+++ flu_finder_src/app.py
@@ -18,4 +18,5 @@ CORS(app)  # Enable CORS to allow frontend requests
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
